b7b374bba5163a4896c8195e83422b38e4ccc30f
Jupiter <email": null>
2015-11-23 16:25:40 -0800

    Establish a saves table

--- db/migrate/20151123162028_create_saves_table.rb
+++ db/migrate/20151123162028_create_saves_table.rb
@@ -1,4 +1,9 @@
 class CreateSavesTable < ActiveRecord::Migration
   def change
+    create_table :saves do |t|
+      t.references :agendas
+      t.references :users
+      t.timestamps
+    end
   end
 end
